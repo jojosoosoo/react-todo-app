3eaf05926e3cd1b42f8381a1ab5061e04f57c137
jojosoosoo <hyeoneegirl@naver.com>
2024-04-03 13:03:22 +0900

    할 일 앱 만들기 UI 구성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./components/TodoList";
+import TodoListItem from "./components/TodoListItem";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/Todoinsert";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
   );
 }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import TodoListItem from "./TodoListItem";
+import "../styles/TodoList.scss";
+
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+    </div>
+  );
+};
+
+export default TodoList;
